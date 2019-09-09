c41d0a14faf5a732a98317d8908ea397bad1da7a
SinRou Chen <appleswill29@gmail.com>
2019-09-09 19:11:55 -0400

    add js file + add nav

---
+++ resources/js/index.js
@@ -0,0 +1,23 @@
+alert("Hello! Welcome to my second website - the first was built with Wix.com for my design artworks.");
+//DOM elements
+const leftbtn = document.getElementById('leftbtn');
+const rightbtn = document.getElementById('rightbtn');
+
+let rotation = 0;
+
+function rotatecat(event){
+    const cat = document.getElementById('pic');
+    rotate(cat, event);
+}
+
+function rotate(element, event){
+    if(event.target.id == 'leftbtn'){
+        rotation = rotation - 15;
+    }else{
+        rotation = rotation + 15;
+    }
+    element.style.transform = 'rotate('+rotation+'deg)';
+}
+
+leftbtn.onclick = rotatecat;
+rightbtn.onclick = rotatecat;
